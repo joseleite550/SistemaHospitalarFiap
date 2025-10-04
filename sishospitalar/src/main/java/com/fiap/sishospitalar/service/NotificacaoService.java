54b992f766f17d58e306ef7bc0662fc3da44c878
joseleite550 <joseleite20022@gmail.com>
2025-10-04 15:33:39 -0300

    ajustes

--- sishospitalar/src/main/java/com/fiap/sishospitalar/service/NotificacaoService.java
+++ sishospitalar/src/main/java/com/fiap/sishospitalar/service/NotificacaoService.java
@@ -1,23 +1,51 @@
 package com.fiap.sishospitalar.service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.fiap.sishospitalar.config.RabbitConfig;
 import com.fiap.sishospitalar.model.Consulta;
+import com.fiap.sishospitalar.repository.ConsultaRepository;
 
 @Service
 public class NotificacaoService {
 
+	private final ConsultaRepository consultaRepository;
+
+	
+    public NotificacaoService(ConsultaRepository consultaRepository) {
+        this.consultaRepository = consultaRepository;
+    }
+    
 	@RabbitListener(queues = RabbitConfig.NOVA_CONSULTA)
 	public void novaConsulta(Consulta consulta) {
 		System.out.println("Nova notificação de consulta para o paciente: " + consulta.getPaciente().getNome());
 		System.out.println("Consulta marcada");
+		enviarEmail(consulta,"Consulta marcada");
 	}
 
 	@RabbitListener(queues = RabbitConfig.ATUALIZAR_CONSULTA)
 	public void atualizarConsulta(Consulta consulta) {
 		System.out.println("Nova notificação de consulta para o paciente: " + consulta.getPaciente().getNome());
 		System.out.println("Consulta atualizada");
+		enviarEmail(consulta,"Consulta atualizada");
+	}
+
+	@Scheduled(fixedRate = 30000)
+	public void enviarLembretesDiarios() {
+		LocalDateTime agora = LocalDateTime.now();
+		LocalDateTime daqui1Dia = agora.plusDays(1);
+
+		List<Consulta> futuras = consultaRepository.findByDataHoraAfter(agora);
+		futuras.stream().filter(c -> !c.getDataHora().isBefore(agora) && c.getDataHora().isBefore(daqui1Dia))
+				.forEach(c -> enviarEmail(c, "Lembrete de consulta amanhã"));
+	}
+
+	private void enviarEmail(Consulta consulta, String assunto) {
+		System.out.println("Email enviado para: " + consulta.getPaciente().getEmail());
 	}
 }
