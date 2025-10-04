4c8844f481657797557cc107504787793d5e2f81
joseleite550 <joseleite20022@gmail.com>
2025-10-03 22:12:01 -0300

    init

---
+++ sishospitalar/src/test/java/com/fiap/sishospitalar/SishospitalarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.sishospitalar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SishospitalarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
