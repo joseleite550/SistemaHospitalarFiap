e25e6df2783475e98ae8d3c4f0441fa6c185a7c0
joseleite550 <joseleite20022@gmail.com>
2025-10-06 21:30:14 -0300

    ajustes

--- sishospitalar/src/main/java/com/fiap/sishospitalar/security/SecurityConfig.java
+++ sishospitalar/src/main/java/com/fiap/sishospitalar/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.fiap.sishospitalar.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,28 +11,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.fiap.sishospitalar.service.UsuarioService;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
 
-	private final UsuarioService usuarioService;
 	private final JwtFilter jwtFilter;
 
-	public SecurityConfig(UsuarioService usuarioService, JwtFilter jwtFilter) {
-		this.usuarioService = usuarioService;
+	public SecurityConfig(JwtFilter jwtFilter) {
 		this.jwtFilter = jwtFilter;
 	}
 
-	@SuppressWarnings("deprecation")
-	@Bean
-	DaoAuthenticationProvider authenticationProvider() {
-		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-		provider.setUserDetailsService(usuarioService);
-		provider.setPasswordEncoder(passwordEncoder());
-		return provider;
-	}
 
 	@Bean
 	AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
@@ -53,7 +41,6 @@ public class SecurityConfig {
 				.authorizeHttpRequests(auth -> auth.requestMatchers("/auth/**").permitAll()
 						.requestMatchers("/h2-console/**").permitAll().anyRequest().authenticated())
 				.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authenticationProvider(authenticationProvider())
 				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
