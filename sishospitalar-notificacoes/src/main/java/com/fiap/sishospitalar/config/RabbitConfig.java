cc634a06eda5d84a0fb231366327975cb40ccb4d
joseleite550 <joseleite20022@gmail.com>
2025-10-06 21:41:15 -0300

    ajustes

--- sishospitalar-notificacoes/src/main/java/com/fiap/sishospitalar/config/RabbitConfig.java
+++ sishospitalar-notificacoes/src/main/java/com/fiap/sishospitalar/config/RabbitConfig.java
@@ -10,55 +10,61 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class RabbitConfig {
 
-    public static final String NOVA_CONSULTA = "nova-consulta";
-    public static final String ATUALIZAR_CONSULTA = "atualizar-consulta";
-    public static final String CONSULTA_EXCHANGE = "consulta-exchange";
-    public static final String CANCELAR_CONSULTA = "cancelar-consulta";
+	public static final String NOVA_CONSULTA = "nova-consulta";
+	public static final String ATUALIZAR_CONSULTA = "atualizar-consulta";
+	public static final String CONSULTA_EXCHANGE = "consulta-exchange";
+	public static final String CANCELAR_CONSULTA = "cancelar-consulta";
 
-    @Bean
-    TopicExchange consultaExchange() {
-        return new TopicExchange(CONSULTA_EXCHANGE);
-    }
+	@Bean
+	TopicExchange consultaExchange() {
+		return new TopicExchange(CONSULTA_EXCHANGE);
+	}
 
-    @Bean
-    Queue filaNovaConsulta() {
-        return new Queue(NOVA_CONSULTA, true);
-    }
+	@Bean
+	Queue filaNovaConsulta() {
+		return new Queue(NOVA_CONSULTA, true);
+	}
 
-    @Bean
-    Queue filaAtualizarConsulta() {
-        return new Queue(ATUALIZAR_CONSULTA, true);
-    }
+	@Bean
+	Queue filaAtualizarConsulta() {
+		return new Queue(ATUALIZAR_CONSULTA, true);
+	}
 
-    @Bean
-    Binding bindingNovaConsulta(@Qualifier("filaNovaConsulta") Queue filaNovaConsulta, TopicExchange consultaExchange) {
-        return BindingBuilder.bind(filaNovaConsulta).to(consultaExchange).with(NOVA_CONSULTA);
-    }
+	@Bean
+	Queue filaCancelarConsulta() {
+		return new Queue(CANCELAR_CONSULTA, true);
+	}
 
-    @Bean
-    Binding bindingAtualizarConsulta(@Qualifier("filaAtualizarConsulta") Queue filaAtualizarConsulta, TopicExchange consultaExchange) {
-        return BindingBuilder.bind(filaAtualizarConsulta).to(consultaExchange).with(ATUALIZAR_CONSULTA);
-    }
-    
+	@Bean
+	Binding bindingNovaConsulta(@Qualifier("filaNovaConsulta") Queue filaNovaConsulta, TopicExchange consultaExchange) {
+		return BindingBuilder.bind(filaNovaConsulta).to(consultaExchange).with(NOVA_CONSULTA);
+	}
+
+	@Bean
+	Binding bindingAtualizarConsulta(@Qualifier("filaAtualizarConsulta") Queue filaAtualizarConsulta,
+			TopicExchange consultaExchange) {
+		return BindingBuilder.bind(filaAtualizarConsulta).to(consultaExchange).with(ATUALIZAR_CONSULTA);
+	}
+
 	@Bean
 	Binding bindingCancelarConsulta(@Qualifier("filaCancelarConsulta") Queue filaCancelarConsulta,
 			TopicExchange consultaExchange) {
 		return BindingBuilder.bind(filaCancelarConsulta).to(consultaExchange).with(CANCELAR_CONSULTA);
 	}
 
-    @Bean
-    Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
+	@Bean
+	Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+		return new Jackson2JsonMessageConverter();
+	}
 
-    @Bean
-    RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
-                                  Jackson2JsonMessageConverter converter) {
-        RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        template.setMessageConverter(converter);
-        return template;
-    }
+	@Bean
+	RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter converter) {
+		RabbitTemplate template = new RabbitTemplate(connectionFactory);
+		template.setMessageConverter(converter);
+		return template;
+	}
 }
